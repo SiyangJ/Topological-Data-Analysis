1a94766aa835cc304c1cfe781e77893403233b38
SiyangJ <siyangj@live.unc.edu>
2018-07-13 08:12:10 -0400

    Create C++ Part

---
+++ Code/INF556_C++/INF556_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INF556_C++
+//
+//  Created by apple on 7/13/18.
+//  Copyright © 2018 UNC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
